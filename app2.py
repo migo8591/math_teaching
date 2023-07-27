a46d2463dd9dfaf2c4242da5554b8831aef54758
miguelmc <mcatedral24@gmail.com>
2023-07-27 10:52:11 -0600

    se crea archivo app2.py

---
+++ app2.py
@@ -0,0 +1,9 @@
+from colorama import  Fore,Style
+country = "Costa Rica..."
+print(Fore.BLUE + country)
+print(Fore.WHITE + country)
+print(Fore.RED + country)
+print(Fore.RED + country)
+print(Fore.WHITE + country)
+print(Fore.BLUE + country)
+print(Style.RESET_ALL)
